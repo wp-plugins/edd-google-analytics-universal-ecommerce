f9b59be1e1ab2c28292795f3d10caca94ef1f6da
danlester <danlester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-13 08:11:50 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/edd-google-analytics-universal-ecommerce/trunk@931660 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ edd_gauniversal_ecommerce.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Plugin Name: EDD Google Analytics Universal Ecommerce
+ * Plugin URI: http://danlester.com/eddgaue
+ * Description: Ecommerce tracking for EDD using Google Universal Analytics - assumes you already create a ga object at the top of the page, and have enabled Ecommerce on your GA dashboard
+ * Version: 1.0
+ * Author: Dan Lester
+ * Author URI: http://wp-glogin.com/
+ * License: GPL3
+ * 
+ * Make sure you are already adding regular Universal Google Analytics code to the top of your page.
+ * The Google object must be called "ga".
+ * For example, use the plugin NK Google Analytics from http://www.marodok.com/nk-google-analytics/
+ * with Tracking code location set to Head
+ * 
+ * You must also have enabled 'Ecommerce' on your Google Analytics dashboard
+ * 
+ * Tested with Easy Digital Downloads version 2.
+ * 
+ */
+
+function edd_ga_ua_ecom_payment_receipt_after_table($payment, $edd_receipt_args) {
+	
+	if ( $edd_receipt_args['payment_id'] ) {
+		?>
+		<script type="text/javascript">
+
+		if (ga) {
+			ga('require', 'ecommerce', 'ecommerce.js');
+			
+			ga('ecommerce:addTransaction', {
+				  'id': '<?php echo esc_js(edd_get_payment_number( $payment->ID )); ?>', // Transaction ID. Required.
+				  'affiliation': '<?php echo esc_js(get_bloginfo('name')); ?>', // Affiliation or store name.
+				  'revenue': '<?php echo $edd_receipt_args[ 'price' ] ? esc_js($edd_receipt_args[ 'price' ]) : '0'; ?>', // Grand Total.
+				  'shipping': '0', // Shipping.
+				  'tax': '<?php echo edd_use_taxes() ? esc_js(edd_payment_tax( $payment->ID )) : '0'; ?>' // Tax.
+				});
+			
+		<?php if ( $edd_receipt_args[ 'products' ] ) {
+				$cart = edd_get_payment_meta_cart_details( $payment->ID, true );
+				if ($cart) {
+					foreach ( $cart as $key => $item ) {
+						if( empty( $item['in_bundle'] ) ) {
+
+							$price_id = edd_get_cart_item_price_id( $item );
+							$itemname = $item['name'];
+							if( ! is_null( $price_id ) ) {
+								$itemname .= ' - '.edd_get_price_option_name( $item['id'], $price_id );
+							}
+				?>
+					ga('ecommerce:addItem', {
+						  'id': '<?php echo esc_js(edd_get_payment_number( $payment->ID )); ?>', // Transaction ID. Required.
+						  'name': '<?php echo esc_js($itemname); ?>',  // Product name. Required.
+						  'sku': '<?php echo esc_js(edd_use_skus() ? edd_get_download_sku( $item['id'] ) : $item['id']); ?>',                 // SKU/code.
+						  //'category': 'Example',  - Category or variation.
+						  'price': '<?php echo esc_js($item['item_price']); ?>', // Unit price.
+						  'quantity': '<?php echo esc_js($item['quantity']); ?>' // Quantity.
+						});
+
+				<?php
+						}
+					}
+				}
+			  }
+			
+		?>
+			ga('ecommerce:send');
+		 }
+		</script>
+		<?php 
+	}
+	
+}
+
+add_action('edd_payment_receipt_after_table', 'edd_ga_ua_ecom_payment_receipt_after_table', 10, 2);
+
+?>
